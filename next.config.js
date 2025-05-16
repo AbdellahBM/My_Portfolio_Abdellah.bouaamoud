df98283fc569784da95cf6919ff499179d9f6b29
AbdellahBM <bouaamoudabdellah@gmail.com>
2025-05-16 22:19:39 +0100

    Add GitHub Pages deployment configuration

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/My_Portfolio_Abdellah.bouaamoud',
+}
+
+module.exports = nextConfig 
